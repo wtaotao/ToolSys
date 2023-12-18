832982235d0bc88e4fe7b2cbb30cd5b7b903ff9a
wtaotao <striving_wang@126.com>
2023-12-18 22:35:10 +0800

    Update leetcode.

--- Nikki/src/com/nikki/leetc/muldynamicplan/RTest123.java
+++ Nikki/src/com/nikki/leetc/muldynamicplan/RTest123.java
@@ -62,9 +62,13 @@ public class RTest123 {
         int buy1 = -prices[0], sell1 = 0;
         int buy2 = -prices[0], sell2 = 0;
         for (int i = 1; i < n; ++i) {
+        	//只进行过一次买操作
             buy1 = Math.max(buy1, -prices[i]);
+            //进行了一次买操作和一次卖操作，即完成了一笔交易
             sell1 = Math.max(sell1, buy1 + prices[i]);
+            //在完成了一笔交易的前提下，进行了第二次买操作
             buy2 = Math.max(buy2, sell1 - prices[i]);
+            //完成了全部两笔交易
             sell2 = Math.max(sell2, buy2 + prices[i]);
         }
         return sell2;
