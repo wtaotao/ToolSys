e32e3a0c42af1a81bb92100c1c67700142faba0b
wtaotao <striving_wang@126.com>
2024-01-17 23:21:56 +0800

    Update comment

--- Nikki/src/com/nikki/leetc/binarysearch/RTest162.java
+++ Nikki/src/com/nikki/leetc/binarysearch/RTest162.java
@@ -25,8 +25,10 @@ public class RTest162 {
 	 * @return
 	 */
     public static int findPeakElement(int[] nums) {
+    	//首先要注意题目条件，在题目描述中出现了nums[-1] = nums[n] = -∞，这就代表着只要数组中存在一个元素比相邻元素大，那么沿着它一定可以找到一个峰值
         int left = 0, right = nums.length - 1;
         for (; left < right; ) {
+        	//获取中间值
             int mid = left + (right - left) / 2;
             if (nums[mid] > nums[mid + 1]) {
                 right = mid;
